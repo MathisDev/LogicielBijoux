94be9ef2c061805ca7e5a3dd40d53475f0c61341
Mathis <75587138+MathisDev@users.noreply.github.com>
2022-06-29 13:41:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class main():
 		window.title("Logiciel Gestion Commande")
 		window.geometry('1500x1000')
 		window.configure(bg="#666363")
+		
 		self.titrerestor = tk.Label(window,text=" REST'OR ",font =('normal',20,'bold'),bg="#666363").place(x=700,y=15)
 		
 
@@ -39,11 +40,11 @@ class main():
 			
 			env = tk.Tk()
 			env.title("Clients")
-			arbre = ttk.Treeview(env, column=("c2", "c3","c4","c5","c6","c7"), show='headings')
+			arbre = ttk.Treeview(env, column=("c1","c2", "c3","c4","c5","c6"), show='headings')
 			
 			conn = sqlite3.connect('base.db')
 			cur = conn.cursor()
-			rqt = "SELECT * FROM Clients;"
+			rqt = "SELECT firmeNom, clientNom, clientPrenom, adresseMail, soldeCompte, telephone FROM Clients;"
 			cur.execute(rqt)
 			rows = cur.fetchall()
 			for row in rows:
@@ -70,10 +71,10 @@ class main():
 			
 			env = tk.Tk()
 			env.title("Commandes")
-			arbre = ttk.Treeview(env, column=("c1", "c2","c4","c7","c8","c9","c10"), show='headings')
+			arbre = ttk.Treeview(env, column=("c1", "c2","c3","c4","c5","c6","c7"), show='headings')
 			conn = sqlite3.connect('base.db')
 			cur = conn.cursor()
-			rqt = "SELECT * FROM Commandes;"
+			rqt = "SELECT dateDepot, idClient, cp, devis, acompte, resterPaye, description FROM Commandes;"
 			cur.execute(rqt)
 			rows = cur.fetchall()
 			for row in rows:
@@ -94,7 +95,35 @@ class main():
 			
 			tk.Button(env, text='Quitter', command=quitter).pack(side=TOP)
 
+
+		# - Recherche un client par son nom de famille et affiche ses informations -
+		def search(entree):
+			tree = ttk.Treeview(window, column=("c1", "c2", "c3","c4","c5","c6","c7","c8","c9"), show='headings')
+			l = ["firmeNom","firmeAdresse","firmeCP","firmeVille","clientNom","clientPrenom","adresseMail","telephone","soldeCompte"]			
+			for i in range(len(l)):
+				tree.column("#"+str(i+1), anchor=tk.CENTER)
+				tree.heading("#"+str(i+1), text=l[i])
+			tree.place(x=0,y=250)
+			if entree.queryString != None:
+				print(entree.queryString)
+			tree.delete(*tree.get_children())
+			conn = sqlite3.connect("base.db")
+			cur = conn.cursor()
+			cur.execute("SELECT firmeNom, firmeAdresse, firmeCP, firmeVille, clientNom, clientPrenom, adresseMail, telephone, soldeCompte FROM Clients WHERE clientNom LIKE ?;", [entree.queryString.get()])
+			fetch = cur.fetchall()
+			for res in fetch:
+				tree.insert('', 'end', values=(res))
+				tree.bind("<Double-1>", get_commandes)
+			cur.close()
+			conn.close()
+	
+
+		# - Permet de cliquer sur les commandes affichées -
 		def get_commandes(event):
+			color1 = "#666363"
+			fg = 13	
+			self.topShowCommande = tk.Toplevel(bg=color1)
+			self.topShowCommande.geometry("1500x700")
 			tree = event.widget
 			citem = tree.focus()
 			val = tree.item(citem)
@@ -105,28 +134,24 @@ class main():
 			fetch = curs.fetchall()
 			print(fetch)
 
-		# - Recherche un client par son nom de famille et affiche ses informations -
-		def search(entree):
-			tree = ttk.Treeview(window, column=("c1", "c2", "c3","c4","c5","c6","c7","c8","c9","c10"), show='headings')
-			l = ["idClient","firmeNom","firmeAdresse","firmeCP","firmeVille","clientNom","clientPrenom","adresseMail","telephone","soldeCompte"]
-			for i in range(len(l)):
-				tree.column("#"+str(i+1), anchor=tk.CENTER)
-				tree.heading("#"+str(i+1), text=l[i])
-			tree.place(x=0,y=250)
-			if entree.queryString != None:
-				print(entree.queryString)
-			tree.delete(*tree.get_children())
-			conn = sqlite3.connect("base.db")
-			cur = conn.cursor()
-			cur.execute("SELECT * FROM Clients WHERE clientNom LIKE ?;", [entree.queryString.get()])
-			fetch = cur.fetchall()
-			for res in fetch:
-				tree.insert('', 'end', values=(res))
-				tree.bind("<Double-1>", get_commandes)
-			cur.close()
-			conn.close()
-	
-		# - Récupération des noms des clients -
+			entreprise = 'Odelor'
+			nom = 'Durant'
+			prenom = 'Jean'
+			compte = '100'
+			tel = '02.32.80.20.39'
+			mail = 'jeanDurant@gmail.com'
+
+			entreCompte = 100
+			compte = str(entreCompte) + 'g'
+			self.LabelFrames = tk.Label(self.topShowCommande,text=fetch[0],font = fg,bg=color1).place(x=20,y=25)
+			self.LabelName = tk.Label(self.topShowCommande,text=fetch[1],bg=color1).place(x=20,y=50)
+			self.LabelFirstName = tk.Label(self.topShowCommande,text=fetch[2],bg=color1).place(x=20,y=75)
+
+			self.LabelMail = tk.Label(self.topShowCommande,text=fetch[3],bg=color1).place(x=20,y=110)
+			self.LabelNumTel = tk.Label(self.topShowCommande,text=fetch[4],bg=color1).place(x=20,y=135)
+
+			self.LabelCompte = tk.Label(self.topShowCommande,text=fetch[5],bg=color1).place(x=340,y=135)
+			self.titreCompte = tk.Label(self.topShowCommande,text='Compte poids : '+fetch[6]+'g',bg=color1).place(x=220,y=135)
 		def recuperer_noms():
 			conn = sqlite3.connect("base.db")
 			cur = conn.cursor()
@@ -136,23 +161,28 @@ class main():
 			conn.close()
 			return [r[0] for r in fetch]
 
-
 		# - Barre de recherche et tableau -
 		barr_rech.queryString = AutocompleteCombobox(window,width=30,font=('Times', 18),completevalues=recuperer_noms())
 		barr_rech.queryString.pack(padx=10,pady=130)
-		tk.Button(window, text='Chercher',highlightbackground = "#666363", command=partial(search,barr_rech)).place(x=0,y=180)
+		tk.Button(window, text='Chercher',highlightbackground = "#666363", command=partial(search,barr_rech)).place(x=900,y=130)
 
 		
 		# - Permet d'ajouter une commande -
 		def windowCommande(self):
+			
+			w = 25
+			l = 120
+			
 			color = "#52575B"
+
 			data = Data() # Instance de Data() qui va nous permettre de stocker les str récupérées grâce à main()
+		
 			self.top = tk.Toplevel(bg=color)
 			self.top.title("Nouvelle Commande")
-			self.top.geometry("400x350")
+			self.top.geometry("400x500")
 
 			self.titreDepotDate = tk.Label(self.top,text="Date de depot",bg=color).grid(row=1)
-			self.titreidClient = tk.Label(self.top,text="num Fiche",bg=color).grid(row=2)
+			self.titreidClient = tk.Label(self.top,text="Identifiant du client",bg=color).grid(row=2)
 			self.titreAdresseLivraison = tk.Label(self.top,text="Adresse de livraison",bg=color).grid(row=3)
 			self.titreCP = tk.Label(self.top,text="Code postale",bg=color).grid(row=4)
 			self.titreVille = tk.Label(self.top,text="Ville",bg=color).grid(row=5)
@@ -184,29 +214,40 @@ class main():
 			data.resterPaye.grid(row=9, column=2)
 			data.Description.grid(row=10, column=2)
 
+
 			# - Permet l'enregistrement de la BDD -
 			def getVarCommande(data,self):
-				#if data.idClient.get()!="" and data.Description.get()!="":
-				conn = sqlite3.connect('base.db')
-				rqt = "INSERT INTO Commandes (dateDepot,idClient,adresseLivraison,cp,ville,prixConvenu,devis,acompte,resterPaye,description) VALUES ('"+data.dateDepot.get()+"','"+data.adresseLivraison.get()+"','"+data.cp.get()+"','"+data.ville.get()+"','"+data.prixConvenu.get()+"','"+data.devis.get()+"','"+data.acompte.get()+"','"+data.resterPaye.get()+"','"+data.Description.get()+"')"
-				cur = conn.cursor()
-				cur.execute(rqt)
-				conn.commit()
-				cur.close()
-				conn.close()
-				self.top.destroy()
 
-			self.btnValide = tk.Button(self.top, text='Valider', highlightbackground =color,command=partial(getVarCommande,data,self)).place(x=250,y=300)
+				if data.idClient.get()!="" and data.Description.get()!="":
+					conn = sqlite3.connect('base.db')
+					rqt = "INSERT INTO Commandes (dateDepot,idClient,adresseLivraison,cp,ville,prixConvenu,devis,acompte,resterPaye,description) VALUES ('"+data.dateDepot.get()+"','"+data.idClient.get()+"','"+data.adresseLivraison.get()+"','"+data.cp.get()+"','"+data.ville.get()+"','"+data.prixConvenu.get()+"','"+data.devis.get()+"','"+data.acompte.get()+"','"+data.resterPaye.get()+"','"+data.Description.get()+"')"
+					cur = conn.cursor()
+					cur.execute(rqt)
+					conn.commit()
+					cur.close()
+					conn.close()
+					self.top.destroy()
+
+			self.btnValide = tk.Button(self.top, text='Valider', highlightbackground =color,command=partial(getVarCommande,data,self)).grid(row=11,column=2,sticky=tk.W,pady=4)
 
 
 		# - Permet d'ajouter un client -
 		def windowClient(self):
+
+			w = 25
+			l = 120
+
 			color = "#52575B"
+
 			data = Data()
+
 			self.top = tk.Toplevel(bg=color)
 			self.top.title("Nouveau client")
-			self.top.geometry("400x350")
-	
+			self.top.geometry("400x300")
+			
+			#self.labelTitre = tk.Label(self.top,text="Nouveaux clients",fg= color,bg=color,font=10).grid(row=0)
+
+			self.titreidClient = tk.Label(self.top,text='Identifiant du client',bg=color).grid(row=1)
 			self.titrefirmeNom = tk.Label(self.top,text='Nom Entreprise',bg=color).grid(row=2)
 			self.titrefirmeAdresse = tk.Label(self.top,text='Adresse Entreprise',bg=color).grid(row=3)
 			self.titrefirmeCP = tk.Label(self.top,text='Code Postale Entreprise',bg=color).grid(row=4)
@@ -217,6 +258,7 @@ class main():
 			self.titreSoldeCompte = tk.Label(self.top,text='Compte poids',bg=color).grid(row=9)
 			self.titreTelephone = tk.Label(self.top,text='Tel',bg=color).grid(row=10)
 			
+			data.idClient = tk.Entry(self.top)
 			data.firmeNom = tk.Entry(self.top)
 			data.firmeAdresse = tk.Entry(self.top)
 			data.firmeCP = tk.Entry(self.top)
@@ -227,6 +269,7 @@ class main():
 			data.soldeCompte = tk.Entry(self.top)
 			data.telephone = tk.Entry(self.top)
 
+			data.idClient.grid(row=1, column=2)
 			data.firmeNom.grid(row=2, column=2)
 			data.firmeAdresse.grid(row=3, column=2)
 			data.firmeCP.grid(row=4, column=2)
@@ -237,64 +280,21 @@ class main():
 			data.soldeCompte.grid(row=9, column=2)
 			data.telephone.grid(row=10, column=2)
 
+
 			# - Permet l'enregistrement de la BDD -
 			def getVarClients(data,self):
-				#if data.idClient.get()!="" and data.firmeNom.get()!="" and data.clientNom.get()!="":
-				conn = sqlite3.connect('base.db')
-				rqt = "INSERT INTO Clients (firmeNom,firmeAdresse,firmeCP,firmeVille,clientNom,clientPrenom,adresseMail,telephone,soldeCompte) VALUES ('"+data.firmeNom.get()+"','"+data.firmeAdresse.get()+"','"+data.firmeCP.get()+"','"+data.firmeVille.get()+"','"+data.clientNom.get()+"','"+data.clientPrenom.get()+"','"+data.adresseMail.get()+"','"+data.telephone.get()+"','"+data.soldeCompte.get()+"');"
-				cur = conn.cursor()
-				cur.execute(rqt)
-				conn.commit()
-				cur.close()
-				conn.close()
-				self.top.destroy()
-			self.btnValide = tk.Button(self.top, text='Valider', highlightbackground =color,command=partial(getVarClients,data,self)).place(x=250,y= 300)
 
-
-		def showClients():
-			color1 = "#666363"
-			fg = 13	
-
-			entreprise = 'Odelor'
-			nom = 'Durant'
-			prenom = 'Jean'
-			compte = '100'
-			tel = '02.32.80.20.39'
-			mail = 'jeanDurant@gmail.com'
-
-			self.topShowCommande = tk.Toplevel(bg=color1)
-			self.topShowCommande.geometry("3000x700")
-			conn = sqlite3.connect('base.db')
-			cur = conn.cursor()
-			rqt = "SELECT * FROM Commandes;"
-			tree = ttk.Treeview(self.topShowCommande,column=("c1", "c2", "c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14"), show='headings')
-			l=["Date de depot","Description","Nom","Code postal","Adresse mail","Telephone","Prenom","Adresse","Ville","Prix convenu", "Devis", "Acompte", "Reste à payer"] 
-			for i in range(len(l)):
-				tree.column("#"+str(i+1), anchor=tk.CENTER)
-				tree.heading("#"+str(i+1), text=l[i])
-			tree.place(x=0,y=250)
-			cur.execute(rqt)
-			rows = cur.fetchall()
-			for row in rows:
-				print(row) 
-				tree.insert("", tk.END, values=row)
-			cur.close()
-			conn.close()
-
-			entreCompte = 100
-			compte = str(entreCompte) + 'g'
-
-			self.LabelFrames = tk.Label(self.topShowCommande,text=entreprise,font = fg,bg=color1).place(x=20,y=25)
-			self.LabelName = tk.Label(self.topShowCommande,text=nom,bg=color1).place(x=20,y=50)
-			self.LabelFirstName = tk.Label(self.topShowCommande,text=prenom,bg=color1).place(x=20,y=75)
-
-			self.LabelMail = tk.Label(self.topShowCommande,text=mail,bg=color1).place(x=20,y=110)
-			self.LabelNumTel = tk.Label(self.topShowCommande,text=tel,bg=color1).place(x=20,y=135)
-
-			self.LabelCompte = tk.Label(self.topShowCommande,text=compte,bg=color1).place(x=340,y=135)
-			self.titreCompte = tk.Label(self.topShowCommande,text='Compte poids : ',bg=color1).place(x=220,y=135)
-	
-		
+				if data.idClient.get()!="" and data.firmeNom.get()!="" and data.clientNom.get()!="":
+					conn = sqlite3.connect('base.db')
+					rqt = "INSERT INTO Clients (idClient,firmeNom,firmeAdresse,firmeCP,firmeVille,clientNom,clientPrenom,adresseMail,telephone,soldeCompte) VALUES ('"+data.idClient.get()+"','"+data.firmeNom.get()+"','"+data.firmeAdresse.get()+"','"+data.firmeCP.get()+"','"+data.firmeVille.get()+"','"+data.clientNom.get()+"','"+data.clientPrenom.get()+"','"+data.adresseMail.get()+"','"+data.telephone.get()+"','"+data.soldeCompte.get()+"');"
+					cur = conn.cursor()
+					cur.execute(rqt)
+					conn.commit()
+					cur.close()
+					conn.close()
+					self.top.destroy()
+        		
+			self.btnValide = tk.Button(self.top, text='Valider', highlightbackground =color,command=partial(getVarClients,data,self)).place(x=230,y= 230)
 
 		# - modif() < showCommand() -			
 		def showCommande():
@@ -383,6 +383,8 @@ class main():
 			self.preResterPaye = tk.Label(self.top,text=txt,width=30,bg="#EEEC7C").place(x=100,y=363)
 
 			self.btnValide = tk.Button(self.top,text="Modifier",bg="#EEEC7C",relief="ridge",highlightbackground = "#EEEC7C",bd=0,command=partial(modif,self)).place(x=330,y=10)
+
+		showCommande()
 
 
 		# - Affiche des boutons permettant d'accéder aux fonctionnalités -
