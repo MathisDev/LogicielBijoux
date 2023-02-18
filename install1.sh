7439c849862cb75d573e89ddd3e4148ce66e7dd1
Mathis <75587138+MathisDev@users.noreply.github.com>
2023-02-18 15:03:38 +0100

    Delete install1.sh

--- install1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install python@3.9
-brew install tcl-tk
-brew install python-tk@3.9
-mkdir /Users/$LOGNAME/Documents/IR8
-cp main.py /Users/$LOGNAME/Documents/IR8/
